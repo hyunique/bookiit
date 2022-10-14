3561b9a7876dd0d762d3e41d2386d700fb800e96
Elena <kunstvanelena@gmail.com>
2022-10-14 13:36:21 +0200

    added New book button, pop up event listener

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 let myLibrary = [];
 
-function Book() {
-
+function Book(author, title, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBooks() {
+const book1 = new Book("Rebecca Solnit", "A field guide to getting lost", 215, "O")
+const book2 = new Book('Tolkien', 'Hobbit', '295', 'X');
 
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    return myLibrary;
 }
+
+function showBooks() {
+    for (let book of myLibrary) {
+        console.table(book)
+    }
+}
+
+
+
+let modal = document.querySelector('.modal')
+let modalBtn = document.querySelector('.addNewBook')
+let closeModal = document.querySelector('.close')
+
+modalBtn.addEventListener('click', function () {
+    modal.style.display = 'block'
+})
+
+// closeModal.addEventListener('click', function (event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none"
+//     }
+// })
